d0a96a81ea94dac85cec13980d2567faa311e4c6
MAranich <114926980+MAranich@users.noreply.github.com>
2024-04-30 18:58:53 +0200

    .

--- main.m
+++ main.m
@@ -11,11 +11,11 @@ clc
 % Input
 sample_path = 'data/blobs.png'; 
 sample = imread(sample_path);
-texture_size = [128, 128] * 2;
+texture_size = [128, 128];
 patch_size = 13; % should be odd (3, 5, 7, etc.)
 tolerance = 0.1;
 
-fprintf("Sample path: %s \nPatch size: [%.0f, %.0f] \t\t Texture size: %d \t\t Tolerance %d\n\n", sample_path, patch_size, texture_size(1), texture_size(2), tolerance); 
+fprintf("Sample path: %s \nPatch size: %d \t\t Texture size: [%d, %d] \t\t Tolerance %d\n\n", sample_path, patch_size, texture_size(1), texture_size(2), tolerance); 
 
 [texture, copy_map] = synthesize_texture(sample, texture_size, patch_size, tolerance);
 
